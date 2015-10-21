18cf7f0c9b5aa4388043359da86735515d1a1349
John Beynon <john@beynon.org.uk>
2015-10-21 13:48:38 +0100

    Use real status

--- status.rb
+++ status.rb
@@ -1,17 +1,22 @@
 require 'sinatra'
 require 'json'
+require 'excon'
 
 get '/' do
+  response = Excon.get('https://status.heroku.com/api/v3/current-status')
+  json = JSON.parse(response.body)
+  production = json['status']['Production']
+  development = json['status']['Development']
   content_type :json
   {
     frames: [
       {
         index: 0,
-        text: "Production Status",
+        text: "Production: #{production}",
         icon: "i972"
       },
         index: 1,
-        text: "Development Status",
+        text: "Development: #{development}",
         icon: "i972"
     ]
   }.to_json
